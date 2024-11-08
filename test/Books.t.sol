93f6aae9320e677ef9c993256a882ce3df2e767c
gypsaman <gypsaman@gmail.com>
2024-11-08 11:37:34 -0600

    final

--- test/Books.t.sol
+++ test/Books.t.sol
@@ -11,12 +11,12 @@ contract TestBooks is Test {
         books = new Books("Programming Foundry", "Cesar Garcia", 100);
     }
 
-    function test_update_pagess() public {
+    function test_update_pages() public {
         books.update_pages(200);
         assertEq(books.get_book().pages, 200);
     }
 
-    function test_get_books() public  {
+    function test_get_book() public  {
         Books.Book memory book = books.get_book();
         assertEq(book.title, "Programming Foundry");
 
