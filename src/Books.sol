25442ff50d2bc8653d1541698c65203f591859db
gypsaman <gypsaman@gmail.com>
2024-11-08 08:41:06 -0600

    with compile error

--- src/Books.sol
+++ src/Books.sol
@@ -12,7 +12,7 @@ contract Books_Contract {
         uint256 pages;
     }
 
-    Book public book;
+    Book public book
 
     modifier onlyOwner {
         require(msg.sender==owner,"You are not the Owner");
